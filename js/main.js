74d1158a996e52a07b4be2a1071ed34277b4b44f
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-08-17 12:17:05 +0300

    add the js folder

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/**
+* @Author: Phillis Kiragu <PKiragu>
+* @Date:   2016-08-16T13:43:37+03:00
+* @Email:  pkiragu@cytonn.com
+* @Last modified by:   PKiragu
+* @Last modified time: 2016-08-17T12:06:13+03:00
+*/
+
+
+// onclick button
+function myFunction1() {
+   document.getElementById("externaldemo").innerHTML = "I am an externaJS";
+
+}
+
+var saveData = function(){
+  var username = document.getElementById('username').value;
+  var email = document.getElementById('email').value;
+
+  console.log("index: ", email.indexOf('a') );
+
+
+  if (username.length === 0){
+    alert('Enter a username');
+  }
+  else if(email.length === 0 || email.indexOf('@') <=0){
+    alert('Enter a valid email');
+  }
+  else{
+    var data = "SUBMIT: username: " + String(username) + " Email: " + String(email);
+    alert(data);
+  }
+
+}
+function say(message) {
+     alert(message);
+  }
